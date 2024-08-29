d44b6658988e1f47f7d5321adb8f9f9ba2b23687
CrissCross20 <165857522+CrissCross20@users.noreply.github.com>
2024-08-28 20:37:53 -0700

    Ejercicio 7. Concatenación de arreglos

---
+++ concatenarArreglos.js
@@ -0,0 +1,7 @@
+//Concatenación de arreglos: Crea dos arreglos de strings y luego concatenarlos en un solo arreglo.
+
+function concatenar (arreglo, arreglo2){
+    return arreglo.concat(arreglo2);
+}
+
+console.log(concatenar(["hola","mundo"],["soy","cristian"]));
